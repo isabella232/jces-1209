a8132422c820f99841333ed162507d5312961bea
AnastasiiaGnes <agnes2@atlassian.com>
2020-10-27 14:57:15 +0200

    JCSP-708: Csv template: board usages: moving BoardUsageFrequency.csv to resources folder

--- custom-vu/src/main/kotlin/jces1209/vu/utils/CsvBoardsReader.kt
+++ custom-vu/src/main/kotlin/jces1209/vu/utils/CsvBoardsReader.kt
@@ -5,39 +5,55 @@ import jces1209.vu.page.boards.view.cloud.*
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import org.openqa.selenium.WebDriver
-import java.io.File
+import java.lang.IllegalStateException
 import java.net.URI
 
 class CsvBoardsReader (
     val driver: WebDriver,
     private val logger: Logger = LogManager.getLogger()
 ) {
-    private val csvPath = "usage-frequency/BoardUsageFrequency.csv"
+    private val csvPropertyFile = "BoardUsageFrequency.csv"
 
-    public fun getBoardsListFromCsv(): MutableList<BoardPage>? {
+    fun getBoardsListFromCsv(): MutableList<BoardPage>? {
         val boardsFromCsv = readFromCsv()
         if (boardsFromCsv.size > 0) {
             return generateBoardsFromCsv(boardsFromCsv)
         } else {
             logger.debug("I cannot parse boards list from csv")
+            println("I cannot parse boards list from csv")
             return null
         }
     }
 
     private fun readFromCsv(): MutableList<CsvBoard> {
         val boardsFromCsv = mutableListOf<CsvBoard>()
-        readCsvByLine(csvPath, boardsFromCsv)
+        var csvContent = String()
+        try {
+            csvContent = this::class.java.getResource("/$csvPropertyFile").readText()
+        } catch (exc: IllegalStateException) {
+            logger.debug("The csv file was not found, skippping")
+        }
+        if (csvContent.isNotEmpty()) {
+            readCsvContentByLine(csvContent, boardsFromCsv)
+        }
         return boardsFromCsv
     }
 
-    private fun readCsvByLine(filename: String, boardsList: MutableList<CsvBoard>) = File(filename).forEachLine { it ->
-        val parsedList = it.split(",").toTypedArray()
-        val id = parsedList.get(0)
-        val frequency = parsedList.get(1).toFloat()
-        val uri = URI(parsedList.get(2))
-        val type = parsedList.get(3).toLowerCase()
-        val csvBoard = CsvBoard(id, frequency, uri, type)
-        boardsList.add(csvBoard)
+    private fun readCsvContentByLine(csvContent: String, boardsList: MutableList<CsvBoard>)  {
+        val csvLines = csvContent.split("\n").toTypedArray()
+        for (line in csvLines) {
+            if (line.isNotEmpty()) {
+                val parsedList = line.split(",").toTypedArray()
+                val id = parsedList.get(0)
+                val frequency = parsedList.get(1).toFloat()
+                val uri = URI(parsedList.get(2))
+                val type = parsedList.get(3).toLowerCase()
+                val csvBoard = CsvBoard(id, frequency, uri, type)
+                boardsList.add(csvBoard)
+            } else {
+                logger.debug("I cannot parse the csv line, skipping...")
+            }
+        }
     }
 
     private fun generateBoardsFromCsv(boardsList: MutableList<CsvBoard>): MutableList<BoardPage> {
