093cd6093adc0383bf212dfc4603f72cc38be85b
anastasiiagnes <72450730+anastasiiagnes@users.noreply.github.com>
2020-10-30 12:55:08 +0200

    Jcsp 708 add csv board usages reading (#103)
    * JCSP-708: Csv template: board usages: adding initial version
    
    * JCSP-708: Csv template: board usages: modifying ViewBoard.kt to read boards from csv
    
    * JCSP-708: Csv template: board usages: moving BoardUsageFrequency.csv to resources folder
    
    * Removing csv file from the repo
    
    * JCSP-708: Csv template: board usages: adding csv file to .gitignore
    
    * JCSP-708: Csv template: board usages: adding frequency handling
    
    * JCSP-708: Csv template: board usages: converting CsvBoardsReader.kt into singleton
    
    * JCSP-708: Csv template: board usages: adding class for testing
    
    * JCSP-708: Csv template: board usages: removing debug logging stuff
    
    * JCSP-708: Csv template: board usages: removing debug logging class
    
    * JCSP-708: Csv template: board usages: moving getBoards and changing totalWeight calculation according to comments
    
    * JCSP-708: Csv template: board usages: moving getBoards and changing totalWeight calculation according to comments
    
    * JCSP-708: Csv template: board usages: updating CsvBoardsReader according to comments
    
    * JCSP-708: Csv template: board usages: adding comments according to the comment

---
+++ custom-vu/src/main/kotlin/jces1209/vu/utils/boards/CsvBoard.kt
@@ -0,0 +1,9 @@
+package jces1209.vu.utils.boards
+
+import java.net.URI
+
+data class CsvBoard(val id: String,
+                    val frequency: Float,
+                    val uri: URI,
+                    val boardType: String
+                    )
