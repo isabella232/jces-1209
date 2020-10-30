1453f9ac46304cb9d1b489fdef9508ce358e3289
ysemenkov <64263766+ysemenkov@users.noreply.github.com>
2020-10-30 14:15:56 +0200

    api: sprint api (#105)

---
+++ custom-vu/src/main/kotlin/jces1209/vu/api/sprint/DcSprintApi.kt
@@ -0,0 +1,16 @@
+package jces1209.vu.api.sprint
+
+import jces1209.vu.api.DcApiClient
+import okhttp3.Credentials
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import java.net.URI
+
+class DcSprintApi(url: URI) : DcApiClient(url), SprintApi {
+
+    override fun deleteSprint(sprintId: String) {
+        TODO("Not yet implemented")
+    }
+}
